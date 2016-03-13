cb2e34fbaad7528879b6fc3ae6a7fe84e939d362
Bart Kampers <github@bartkampers.nl>
2016-03-13 20:32:27 +0100

    Analog input simulator fix

--- Platforms/MacOS/AnalogInput.c
+++ Platforms/MacOS/AnalogInput.c
@@ -2,9 +2,8 @@
 
 #include <stdint.h>
 
-#define ADC1_DR_Address ((uint32_t) 0x4001244C)
 
-#define NUMBER_OF_ADC_PORTS 5
+#define NUMBER_OF_ADC_PORTS 8
 
 
 char* INVALID_ADC_INDEX = "Invalid ADC index";
@@ -19,13 +18,13 @@ void InitAnalogInput()
 
 Status GetAnalogValue(int index, int* value)
 {
-	if ((0 <= index) && (index < NUMBER_OF_ADC_PORTS))
-	{
-		*value = (int) ADCConvertedValues[index];
-		return OK;
-	}
-	else
-	{
-		return INVALID_ADC_INDEX;
-	}
+    if ((0 <= index) && (index < NUMBER_OF_ADC_PORTS))
+    {
+        *value = (int) ADCConvertedValues[index];
+        return OK;
+    }
+    else
+    {
+        return INVALID_ADC_INDEX;
+    }
 }
